8f55dcd30cd89bff5686731b0ae4dea0edd3ec52
0KateloK0 <katelkin2003@mail.ru>
2021-12-25 22:14:13 +0300

    Class Line, minor changes to Point

--- geometry.h
+++ geometry.h
@@ -21,16 +21,20 @@ struct Point {
     // Точка по сути своей это вектор с началом (0, 0) и координатами (x, y). Посему точка будет иметь все функции
     // 2д вектора, а также возможность нахождения векторного произведения, которое будет возвращать ЗНАКОВОЕ значение
     // модуля результирующего вектора
-    Point operator += (Point const & p) {
+    Point & operator += (Point const & p) {
         x += p.x; y += p.y;
         return *this;
     }
-    Point operator -= (Point const & p) {
+    Point & operator -= (Point const & p) {
         x -= p.x; y -= p.y;
         return *this;
     }
-    Point operator - () {
+    Point & operator - () {
         x = -x; y = -y;
+        return *this;
+    }
+    Point & operator *= (double l) {
+        x *= l; y *= l;
         return *this;
     }
 
@@ -49,8 +53,16 @@ Point operator - (Point const & p) {
     Point cpy = p;
     return -cpy;
 }
+Point operator * (Point const & p, double l) {
+    Point cpy(p);
+    return cpy *= l;
+}
+Point operator * (double l, Point const & p) {
+    Point cpy(p);
+    return cpy *= l;
+}
 std::ostream & operator << (std::ostream & cout, Point const & p) {
-    cout << p.x << ' ' << p.y;
+    cout << p.x << ' ' << p.y << '\n';
     return cout;
 }
 
@@ -63,3 +75,34 @@ double operator * (Point const & p1, Point const & p2) {
 double operator ^ (Point const & p1, Point const & p2) {
     return p1.x * p2.x + p1.y * p2.y;
 }
+
+class Line {
+public:
+    Line () = default;
+    Line (Point const & p0, Point const & p1) :
+            A(p0.y-p1.y), B(p1.x - p0.x), C(p0.x * p1.y - p0.y * p1.x) {}
+    Line (double k, double b) : A(-k), B(1), C(-b) {}
+
+    [[nodiscard]] bool belongs (Point const & p) const {
+        return A * p.x + B * p.y + C == 0;
+    }
+
+    bool operator == (Line const & right) const {
+        return Matrix<2, 3>{{A, B, C}, {right.A, right.B, right.C}}.rank() != 2;
+    }
+
+    bool operator != (Line const & right) const { return !(*this == right); }
+
+    // линия хранится в виде Ax+By+C=0
+    double A = -1;
+    double B = 1;
+    double C = 0;
+
+    friend std::ostream & operator << (std::ostream & cout, Line const & l);
+};
+
+std::ostream & operator << (std::ostream & cout, Line const & l) {
+    cout << l.A << ' ' << l.B << ' ' << l.C << '\n';
+    return cout;
+}
+
